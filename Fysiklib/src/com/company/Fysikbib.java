ae365e70493545387e0fc5797308ebbcba879946
minichell <dorna-michell.dadelahi@elev.ga.ntig.se>
2019-11-13 16:13:35 +0100

    hej!

---
+++ Fysiklib/src/com/company/Fysikbib.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Fysikbib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println(fahrenheitToCelsius(50));
+	System.out.println(kelvinToCelsius(0));
+	
+
+	}
+	
+	/**
+ * En metod för att konvertera temperaturen från fahrenheit till celsius
+	 * @param fahrenheit
+	 * @return
+	 */
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		double celsius = (fahrenheit - 32)*5/9;
+
+		return celsius;
+	}	
+	
+	
+	/**
+ * En metod för att konvertera temperaturen från kelvin till celsius
+	 * @param kelvin
+	 * @return
+	 */
+		public static double kelvinToCelsius(double kelvin) {
+			double celsius = kelvin - 273.15;
+			return celsius;
+	}
+}
