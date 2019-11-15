9dfafee43d03ef94de817ebeface46d18d2fe3b4
minichell <dorna-michell.dadelahi@elev.ga.ntig.se>
2019-11-15 11:34:01 +0100

    Metod 1 är avklarad.

--- Fysiklib/src/com/company/Fysikbib.java
+++ Fysiklib/src/com/company/Fysikbib.java
@@ -5,30 +5,21 @@ public class Fysikbib {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println(fahrenheitToCelsius(50));
-	System.out.println(kelvinToCelsius(0));
+
 	
 
 	}
 	
 	/**
- * En metod för att konvertera temperaturen från fahrenheit till celsius
-	 * @param fahrenheit
-	 * @return
+	 * En metod för att konvertera temperaturen från fahrenheit till celsius
+	 * @param fahrenheit - variabel som är temperaturen
+	 * @return - Celsius
 	 */
 	public static double fahrenheitToCelsius(double fahrenheit) {
 		double celsius = (fahrenheit - 32)*5/9;
-		int hej = 0;
 		return celsius;
 	}	
 	
 	
-	/**
- * En metod för att konvertera temperaturen från kelvin till celsius
-	 * @param kelvin
-	 * @return
-	 */
-		public static double kelvinToCelsius(double kelvin) {
-			double celsius = kelvin - 273.15;
-			return celsius;
-	}
+
 }
