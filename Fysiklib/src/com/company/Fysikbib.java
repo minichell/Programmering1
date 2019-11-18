32e2d6b221bcee824f2ada252e63c563f4e7cb09
minichell <dorna-michell.dadelahi@elev.ga.ntig.se>
2019-11-18 10:56:28 +0100

    Metod 4 är avklarad.

--- Fysiklib/src/com/company/Fysikbib.java
+++ Fysiklib/src/com/company/Fysikbib.java
@@ -1,27 +1,32 @@
 package com.company;
 
-public class Fysikbib {
+import static com.company.FluidTable.H20;
+
+public class Fysikbib<thePressure> {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println(fahrenheitToCelsius(50));
-	System.out.println(kelvinToCelsius(0));
-	System.out.println(fluidPressure(FluidTable.WATER, 10));
+		System.out.println(fahrenheitToCelsius(50));
+		System.out.println(kelvinToCelsius(0));
+		System.out.println(fluidPressure(FluidTable.WATER, 10));
+		System.out.println(pressureUnderWater(10));
 
 	}
-	
+
 	/**
 	 * En metod för att konvertera temperaturen från fahrenheit till celsius
+	 *
 	 * @param fahrenheit - variabel som är temperaturen
 	 * @return - Celsius
 	 */
 	public static double fahrenheitToCelsius(double fahrenheit) {
-		double celsius = (fahrenheit - 32)*5/9;
+		double celsius = (fahrenheit - 32) * 5 / 9;
 		return celsius;
 	}
 
 	/**
 	 * En metod för att konvertera temperaturen från kelvin till celsius
+	 *
 	 * @param kelvin - En variabel som är temperaturen
 	 * @return - Celsius
 	 */
@@ -32,15 +37,25 @@ public class Fysikbib {
 
 	/**
 	 * En metod som räknar ut vätsketrycket i en vätska vid ett visst djup.
+	 *
 	 * @param fluid - En variabel som är densitetet
+	 * @param deep  - En variabel som är höjden
+	 * @return - thePressure
+	 */
+	public static double fluidPressure(FluidTable fluid, double deep) {
+		double thePressure = 0;
+		thePressure = fluid.density * 9.82 * deep;
+		return thePressure;
+
+	}
+
+	/**
+	 * En metod som räknar ut vätsketrycket i vatten vid ett visst djup
 	 * @param deep - En variabel som är höjden
 	 * @return - thePressure
 	 */
-    public static double fluidPressure(FluidTable fluid, double deep){
-	    double thePressure = 0;
-	    thePressure = fluid.density*9.82*deep;
+	public static double pressureUnderWater(double deep) {
+		double thePressure = H20.density*9.82*deep;
 		return thePressure;
-
-    }
-
+}
 }
