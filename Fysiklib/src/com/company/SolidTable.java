77fa851d6292ac87b206e51119bc37f98228565c
minichell <dorna-michell.dadelahi@elev.ga.ntig.se>
2019-11-29 11:58:55 +0100

    lagt till 2 ämnen inom solidtable

--- Fysiklib/src/com/company/SolidTable.java
+++ Fysiklib/src/com/company/SolidTable.java
@@ -3,7 +3,9 @@ package com.company;
 public enum SolidTable {
 
 	ICE(0.92, 2.2, 0, 334),
-	IRON(7.87, 0.45, 1538, 276)
+	IRON(7.87, 0.45, 1538, 276),
+	GULD(19.3, 0.13, 1064, 63),
+	KOPPAR(8.96, 0.39, 1085, 206)
 	;
 
 	double density;
