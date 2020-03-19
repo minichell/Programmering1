8b0d04d727ee7e0dfc29755a02c11e8d6daf0333
minichell <54796879+minichell@users.noreply.github.com>
2020-03-19 13:03:38 +0100

    skriver in om hur spelet går till

--- GuessingNumber/src/com/company/Main.java
+++ GuessingNumber/src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-        static Scanner inputFromPlayer = new Scanner (System.in);
+        static Scanner inputFromPlayer = new Scanner(System.in);
 
         protected static int choosingLevel; //the level the player chose
         protected static int minimunNumber; //the lowest number
@@ -15,5 +15,30 @@ public class Main {
         protected static int guessedNumber; //that number that the player guessed
         protected static boolean run = true; //the program will run when is it true
         protected static int playAgainOrQuit; //the player choosing if he wants to play again or not
-    }
 
+        public static void main(String[] args) {
+
+                guessingNumberGame();
+        }
+
+        protected static void guessingNumberGame(){
+
+                        while (run) {
+                                System.out.println("Welcome to guessing number game");
+                                System.out.println();
+                                System.out.println("On this game you have only 10 chances\"to guess right.\n" +
+                                        "If you guess right, you win the game.\n" +
+                                        "If you don't don't have guesses left, you lose the game.");
+                                System.out.println();
+                                System.out.println("Choose the level that you want to play: \n 1. Easy level\n + 2. Medium level\n +" +
+                                        "3. difficulty level");
+                                System.out.println();
+                                System.out.println("Choose the number down below");
+
+
+                        }
+
+
+                }
+
+        }
