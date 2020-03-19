1dbfb339bec743b15a685c0c1bb05d568f2e849d
minichell <54796879+minichell@users.noreply.github.com>
2020-03-19 13:06:33 +0100

    spelaren skriver in vilken level som personen vill köra

--- GuessingNumber/src/com/company/Main.java
+++ GuessingNumber/src/com/company/Main.java
@@ -36,6 +36,9 @@ public class Main {
                                 System.out.println("Choose the number down below");
 
 
+                choosingLevel = inputFromPlayer.nextInt();
+
+
                         }
 
 
