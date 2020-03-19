4ccdf6ce3dc00831aad0bf958f373c5fdef16c8b
minichell <54796879+minichell@users.noreply.github.com>
2020-03-19 12:34:21 +0100

    har lagt till metoderna + boolean = true

--- GuessingNumber/src/com/company/Main.java
+++ GuessingNumber/src/com/company/Main.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class Main {
 
         static Scanner inputFromPlayer = new Scanner (System.in);
+
+        protected static int choosingLevel; //the level the player chose
+        protected static int minimunNumber; //the lowest number
+        protected static int maximumNumber; //the heighest number
+        protected static int rightNumber; //the random number
+        protected static int guessesLeft; //the number of guesses that player has left
+        protected static int numberOfGuesses; // the number of guesses that player gets from the beginning
+        protected static int guessedNumber; //that number that the player guessed
+        protected static boolean run = true; //the program will run when is it true
     }
 
