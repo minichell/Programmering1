726aba14115825539f8734be2c6fb9584afcabfb
minichell <54796879+minichell@users.noreply.github.com>
2020-03-19 15:48:07 +0100

    skapade metoder

---
+++ GuessingNumberGameee/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner inputFromPlayer = new Scanner(System.in);
+
+    protected static int choosingLevel;
+    protected static int minimumNumber;
+    protected static int maximumNumber;
+    protected static int rightNumber;
+    protected static int guessesLeft;
+    protected static int numberOfGuesses;
+    protected static int guessedNumber;
+    protected static int playAgainOrQuit;
+
+
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+
