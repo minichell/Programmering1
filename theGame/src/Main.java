7ac2946ce8b26771ffb6d8d45dae6cea877a1e5d
minichell <54796879+minichell@users.noreply.github.com>
2020-03-20 16:27:23 +0100

    skapade metoderna som jag kommer använda utav

--- theGame/src/Main.java
+++ theGame/src/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     static Scanner inputFromPlayer = new Scanner(System.in);
 
+    public static int choosingLevel;
+    public static int minimumNumber = 0;
+    public static int maximumNumber;
+    public static int rightNumber;
+    public static int guessesLeft;
+    public static int numberOfGuesses = 10;
+    public static int guessedNumber;
+    public static int playAgainOrQuit;
+    public static boolean run = true;
+
 
 }
