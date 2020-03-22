28d9b76563868d8f927f69a0266509b0f4b6d820
minichell <54796879+minichell@users.noreply.github.com>
2020-03-22 18:35:22 +0100

    spelare kan välja vilken level som personen vill spela

--- theGame/src/Main.java
+++ theGame/src/Main.java
@@ -1,11 +1,14 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    static Scanner inputFromPlayer = new Scanner(System.in);
+    static Scanner inputFromPlayer = new Scanner(System.in); //the player have able to put an input
 
-    public static int choosingLevel;
+    public static int chooseLevel; //the player that player can choose between
     public static int minimumNumber = 0;
     public static int maximumNumber;
     public static int rightNumber;
@@ -13,9 +16,8 @@ public class Main {
     public static int numberOfGuesses = 10;
     public static int guessedNumber;
     public static int playAgainOrQuit;
-    public static boolean run = true;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         guessingNumberGame();
     }
 
@@ -27,22 +29,61 @@ public class Main {
 
     }
 
+    public static void guessingNumberGame() {
 
-    public static void guessingNumberGame(){
-        System.out.println("Welcome to guessing number game!");
+            System.out.println("Welcome to guessing number game!");
+            System.out.println();
+            System.out.println("On this game you have only 10 chances to guess right\n" +
+                    "If you guess right, you win the game\n" +
+                    "If you don't have any guesses left, you lose the game.");
         System.out.println();
-        System.out.println("On this game you have only 10 chances to guess right\n" +
-                "If you guess right, you win the game\n" +
-                "If you don't have any guesses left, you lose the game.");
-        System.out.println();
+        System.out.println("Choose the level that you want to play:\n"+
+                    "1.Easy Level\n"+
+                    "2.Medium Level\n"+
+                    "3.Difficulty Level");
+            System.out.println();
 
+            choosingLevel();
     }
 
-    public static void easyLevel(){
+
+
+    public static void playingAgainOrNot() {
+        System.out.println("Do you want to play again?");
+        System.out.println();
+        System.out.println("Write the number that you want");
+        System.out.println("1.Yes\n"+"2.No");
+        System.out.println();
+
+        playAgainOrQuit = inputFromPlayer.nextInt();
+
+        if (playAgainOrQuit == 1) {
+                guessingNumberGame();
+
+        } else if (playAgainOrQuit == 2) {
+            System.out.println("Goodbye!");
+        }
+    }
+
+    public static void choosingLevel() {
+        chooseLevel = inputFromPlayer.nextInt();
+        if (chooseLevel == 1) {
+            easyLevel();
+        } else if (chooseLevel == 2) {
+            mediumLevel();
+        } else if (chooseLevel == 3) {
+            difficultyLevel();
+        }
+    }
+
+
+    public static void easyLevel() {
         System.out.flush();
+        numberOfGuesses = 10;
         System.out.println("You have only 10 guesses");
         System.out.println("You are going to guess between 0-10");
         maximumNumber = 10;
+        minimumNumber = 0;
 
         systemOfTheGame();
     }
@@ -53,6 +94,7 @@ public class Main {
         numberOfGuesses = 10;
         System.out.println("You are going to guess between 0-50");
         maximumNumber = 50;
+
         systemOfTheGame();
 
     }
@@ -64,32 +106,36 @@ public class Main {
         System.out.println("You are going to guess between 0-100");
         minimumNumber = 0;
         maximumNumber = 100;
+
         systemOfTheGame();
     }
-    public static void systemOfTheGame(){
+
+    public static void systemOfTheGame() {
         rightNumber = answerNum(maximumNumber, minimumNumber);
+             System.out.println("Guess a number");
         for (int i = 1; i <= numberOfGuesses; i--) {
             guessedNumber = inputFromPlayer.nextInt();
             numberOfGuesses--;
 
-            System.out.println("Guess");
-            guessedNumber = inputFromPlayer.nextInt();
             if (guessedNumber == rightNumber) {
-                System.out.println("Congrats!\n " +
-                        "You won the game");
-                System.out.println();
-            }
-            else if(guessedNumber < rightNumber){
+                System.out.println("Congrats!\n" +
+                        "You won the game!");
+                playingAgainOrNot();
+
+            } else if (guessedNumber < rightNumber) {
                 System.out.println("Sorry you guessed wrong\n" +
                         "the number is higher\n" +
-                        "You have "  + numberOfGuesses +  " guesses left");
-            }
-            else if(guessedNumber > rightNumber){
+                        "You have " + numberOfGuesses + " guesses left");
+            } else if (guessedNumber > rightNumber) {
                 System.out.println("Sorry you guessed wrong\n" +
                         "the number is lower\n" +
-                        "you have "  + numberOfGuesses +  " guesses left");
+                        "you have " + numberOfGuesses + " guesses left");
 
+            } if (numberOfGuesses == 0) {
+                System.out.println("Sorry you lost the game");
+                playingAgainOrNot();
             }
+
         }
     }
 }
