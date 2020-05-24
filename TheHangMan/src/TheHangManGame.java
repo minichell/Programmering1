29b2b485715393d7a6b027406c68c818ffde63ca
minichell <54796879+minichell@users.noreply.github.com>
2020-05-24 13:57:06 +0200

    skapade start position metoden

--- TheHangMan/src/TheHangManGame.java
+++ TheHangMan/src/TheHangManGame.java
@@ -12,6 +12,16 @@ public class TheHangManGame {
     }
 
 
+    public static void startPosition(){
+        System.out.println(" Welcome to Hangman Game!");
+        System.out.println();
+        System.out.println("You will guess a word by guessing letter. \n" +
+                "Every time you guess wrong it will build pieces of the hangman. \n" +
+                "If the hangman is complete, you lose the game. \n" +
+                "If you guess the word right, you win the game" );
+        System.out.println();
+    }
+
     public static void TheGame(){
         String [] guesses = {"blue" , "red" , "pink"};
         boolean wePlaying = true;
