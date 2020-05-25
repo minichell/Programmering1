e9b8254981be9d02143b7e7f57d6d239870879f0
minichell <54796879+minichell@users.noreply.github.com>
2020-05-25 20:46:28 +0200

    jag skapade metoder: easy,medium och även difficult och startpositon och fyllde den med kod

--- theHangManGame/src/theHangManGame.java
+++ theHangManGame/src/theHangManGame.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class theHangManGame {
-    Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
     public static Random random = new Random();
     public static char[] theCorrectGuesses;
     public static char[] randomWord;
@@ -10,9 +10,13 @@ public class theHangManGame {
 
 
     public static void main(String[] args){
+        startPosition();
         randomWord();
 
     }
+
+
+
 
     public static void randomWord(){
         String [] guesses = {"computer" , "flower" , "phone" , "shoes" , "football" , "chips" , "clothes"};
@@ -29,6 +33,47 @@ public class theHangManGame {
         System.out.println(theCorrectGuesses);
     }
 
+    public static int isNextIntValid(){
+        while (true){
+            try{
+                return input.nextInt();
+            }
+            catch (Exception e){
+                System.out.println("Try to write a number pls");
+                input.nextLine();
+                continue;
+            }
+        }
+    }
+    public static void startPosition() {
+        System.out.println("Welcome to the HangmanGame! \n" +
+                "In this game you will guess a word. \n" +
+                "Pls choose a level: \n" +
+                "1.Easy Level\n" +
+                "2.Medium Level\n" +
+                "3.Difficulty level");
+
+        while (true) {
+            int theLevel = isNextIntValid();
+            if(theLevel == 1){
+                easyGame();
+                break;
+            } else if(theLevel == 2){
+                mediumGame();
+                break;
+            } else if(theLevel == 3){
+                difficultyGame();
+                break;
+            } else {
+                System.out.println("Pls right 1 or 2 or 3 pls!");
+            }
+        }
+
+    }
+
+    public static void easyGame(){}
+    public static void mediumGame(){}
+    public static void difficultyGame(){}
 
     public static void hangManGame(){
 
