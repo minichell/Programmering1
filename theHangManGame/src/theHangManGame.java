08fa7087c512ae43909ab7698fc85924c39c4e68
minichell <54796879+minichell@users.noreply.github.com>
2020-05-26 22:27:50 +0200

    skapade en metod där spelaren ska kunna bara skriva in en bokstav

--- theHangManGame/src/theHangManGame.java
+++ theHangManGame/src/theHangManGame.java
@@ -1,29 +1,32 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class theHangManGame {
     static Scanner input = new Scanner(System.in);
     public static Random random = new Random();
+    public static char theGuess;
     public static char[] theCorrectGuesses;
     public static char[] randomWord;
     public static int wordLength;
     public static int numberOfWrongGuesses;
+    public static ArrayList<Character> incorrectGuesses = new ArrayList<Character>();
 
     public static void main(String[] args){
+        randomWord();
         startPosition();
 
     }
 
-
     public static void randomWord(){
         String [] guesses = {"computer" , "flower" , "phone" , "shoes" , "football" , "chips" , "clothes"};
-        char[] randomWord = guesses[random.nextInt(guesses.length)].toCharArray();
+        randomWord = guesses[random.nextInt(guesses.length)].toCharArray();
         wordLength = randomWord.length;
         createGuesses();
     }
 
     public static void createGuesses(){
-        char[] theCorrectGuesses = new char[wordLength];
+        theCorrectGuesses = new char[wordLength];
        for(int i = 0; i < theCorrectGuesses.length; i++){
            theCorrectGuesses[i] = '-';
        }
@@ -81,10 +84,160 @@ public class theHangManGame {
     public static void difficultyGame(){
         numberOfWrongGuesses = 6;
         System.out.println("The hangman will start hanging if you guess wrong");
+        hangManGame();
     }
+
+    public static char allowOnlyLetter(){
+        boolean allowLetter = true;
+        while (allowLetter){
+            theGuess = input.next().charAt(0);
+            String player = String.valueOf(theGuess);
+            if(player.length() > 1){
+                System.out.println("Pls write only one letter");
+            } else if(!player.matches("[a-zA-Z]+")){
+                System.out.println("Pls write a letter");
+            } else if(player.matches("[a-zA-Z]+")){
+                allowLetter = false;
+            }
+        }
+        return theGuess;
+    }
+
 
     public static void hangManGame(){
         System.out.println("The word is:");
         randomWord();
+        System.out.println("The letter is " + wordLength + " long");
+
+        boolean guessedWord = false;
+        while(!guessedWord && numberOfWrongGuesses !=0){
+            System.out.println("Guess a letter");
+            theGuess = allowOnlyLetter();
+            isTheLetterCorrect();
+            guessedWord = ifTheWordIsGuessed();
+            System.out.println("The word: " + Arrays.toString(theCorrectGuesses));
+            System.out.println("Your wrong guesses: " + incorrectGuesses);
+        }
+        if(numberOfWrongGuesses==0 || guessedWord!= true) {
+            System.out.println("You lost");
+            System.out.println("Your wrong guesses are: " + incorrectGuesses);
+            System.out.println("The word was: " + Arrays.toString(randomWord));
+        }
+        playAgainOrNot();
     }
+
+    public static void isTheLetterCorrect(){
+        boolean correctInput = false;
+        for(int i = 0; i < randomWord.length ; i++){
+            if (randomWord[i] == theGuess) {
+                System.out.println("correct guess");
+                theCorrectGuesses[i] = theGuess;
+                correctInput = true;
+            }
+        }
+        if (correctInput!= true) {
+            incorrectGuesses.add(theGuess);
+            numberOfWrongGuesses--;
+            printOutHangMan();
+        }
+    }
+
+    public static boolean ifTheWordIsGuessed(){
+        for (int i = 0; i < theCorrectGuesses.length; i++) {
+            if (theCorrectGuesses[i] == '-') {
+                return false;
+            }
+        }
+        System.out.println("The word has been guessed");
+        return true;
+        }
+
+        public static void playAgainOrNot(){
+            System.out.println("Do you want to play again or not?");
+            System.out.println("1.Yes\n" +
+                                "2.No");
+            while (true){
+                int playAgain = isNextIntValid();
+                if(playAgain == 1){
+                    startPosition();
+                    break;
+                } else if (playAgain == 2){
+                    System.out.println("Cya later bye!");
+                    break;
+                } else {
+                    System.out.println(" pls answer 1 or 2!");
+                }
+            }
+        }
+
+
+
+    public static void printOutHangMan() {
+        if (numberOfWrongGuesses == 6) {
+            System.out.println("The hangman is hanging!");
+            System.out.println("  +---+\n" +
+                    "   |  |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========");
+        }
+        else if(numberOfWrongGuesses == 5) {
+            System.out.println("  +---+\n" +
+                    "   |   |\n" +
+                    "   O   |\n" +
+                    "       |\n" +
+                    "       |\n" +
+                    "       |\n" +
+                    "==========");
+        }
+        else if(numberOfWrongGuesses == 4) {
+            System.out.println("  +---+\n" +
+                    "   |   |\n" +
+                    "   O   |\n" +
+                    "   |   |\n" +
+                    "       |\n" +
+                    "       |\n" +
+                    "==========");
+        }
+        else if(numberOfWrongGuesses == 3) {
+            System.out.println("  +---+\n" +
+                    "   |   |\n" +
+                    "   O   |\n" +
+                    "  /|   |\n" +
+                    "       |\n" +
+                    "       |\n" +
+                    "==========");
+        }
+        else if(numberOfWrongGuesses == 2) {
+            System.out.println("The hangman is on..");
+            System.out.println("  +---+\n" +
+                    "   |   |\n" +
+                    "   O   |\n" +
+                    "  /|\\ |\n" +
+                    "       |\n" +
+                    "       |\n" +
+                    "===========");
+        }
+        else if(numberOfWrongGuesses == 1) {
+            System.out.println("  +---+\n" +
+                    "   |   |\n" +
+                    "   O   |\n" +
+                    "  /|\\ |\n" +
+                    "  /    |\n" +
+                    "       |\n" +
+                    "==========");
+        }
+        else if(numberOfWrongGuesses == 0) {
+            System.out.println("  +---+\n" +
+                    "   |   |\n" +
+                    "   O   |\n" +
+                    "  /|\\ |\n" +
+                    "  / \\ |\n" +
+                    "       |\n" +
+                    "==========");
+        }
+    }
+
 }
